3f101746cb7f5705b1b9006479ca5ab787e1dac9
unknown <firdaagustina852@gmail.com>
2026-06-25 02:47:14 +0700

    initial commit : penambahan sensor api pada smart home - Firda Dwi Agustina

--- projectIOT_UAS.ino
+++ projectIOT_UAS.ino
@@ -1,8 +1,13 @@
 #include <SoftwareSerial.h>
 
 
+// Sensor Api
+int sensorPin = 8;
+int sensorValue = 0;
 
-
+//LED & Buzzer untuk Indikator Kebakaran 
+int ledMerah = 2;
+int buzzer = 5;
 
 
 //LED LAMPU CONTROL
@@ -10,14 +15,35 @@ int ledPutih = 3;
 int ledBening = 4;
 
 void setup() {
+  Serial.begin(9600);
 
-
-pinMode(ledPutih, OUTPUT);
-pinMode(ledBening, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  pinMode(ledMerah, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(ledPutih, OUTPUT);
+  pinMode(ledBening, OUTPUT);
 
 }
 
-void loop() {
+void loop(){
+// DETEKSI SENSOR API
+  sensorValue = digitalRead(sensorPin);
+
+  if(sensorValue == LOW);{
+    digitalWrite (ledMerah, HIGH);
+    digitalWrite (buzzer, HIGH);
+    if (millis() - waktuLama >= 500){
+      Serial.Println("⚠️ BAHAYA! Kebakaran Terdeteksi!");
+      waktuLama = millis();
+    }
+  } else{
+    digitalWrite(ledMerah, LOW);
+    digitalWrite(buzzer, LOW);
+    if (millis() - waktuLama >= 2000){
+      Serial.Println("🟢 Status: Suhu & Lingkungan Aman.");
+      waktuLama = millis();
+    }
+  }
 
 //LED LAMPU CONTROL
 //Ketik 2 :nyalakan lampu LED putih
