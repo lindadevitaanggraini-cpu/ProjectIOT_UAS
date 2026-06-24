660c8acca3eebf1d356345f979eaa34a4439111b
Elsa Melania Putri <melaniaelsa011@gmail.com>
2026-06-25 03:28:35 +0700

    initial commit : penambahan garasi mobil smart home - elsa

--- projectIOT_UAS.ino
+++ projectIOT_UAS.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-
+#include <Servo.h>
 
 // Sensor Api
 int sensorPin = 8;
@@ -9,10 +9,19 @@ int sensorValue = 0;
 int ledMerah = 2;
 int buzzer = 5;
 
-
 //LED LAMPU CONTROL
 int ledPutih = 3;
 int ledBening = 4;
+
+// Servo Motor untuk Garasi
+Servo myServo;
+int servoPin = 6;
+
+// Perbaikan Variabel untuk menyimpan posisi sudut terakhir servo garasi
+int posisiGarasi = 0; // Mulai dari 0 (tertutup)
+
+//Variabel pembantu pembatas waktu cetak di terminal
+unsigned long waktuLama = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -23,6 +32,11 @@ void setup() {
   pinMode(ledPutih, OUTPUT);
   pinMode(ledBening, OUTPUT);
 
+  //Setting awal servo garasi
+  myServo.attach(servoPin);
+  myServo.attach(posisiGarasi); // Set awal halus (0 derajat)
+
+  Serial.println("--- SISTEM SMART HOME GERAKAN HALUS AKTIF");
 }
 
 void loop(){
@@ -45,6 +59,40 @@ void loop(){
     }
   }
 
+  // ---SUB-SISTEM 2: KONTROL SERIAL (DENGAN EFEK SMOOTH SERVO)---
+  if(Serial.available() > 0) {
+    char hasil = Serial.read();
+
+//Ketik 1: Buka Garasi secara perlahan
+if(hasil == '1') {
+  if (posisiGarasi!= 90) {//Hanya bergerak jika posisi saat ini belum terbuka
+    Serial.println("Perintah: Membuka Pintu Garasi secara perlahan...");
+
+    // Loop menaikkan sudut dari posisi terakhir menuju 90 derajat
+    for (int i = posisisGarasi;  <= 90; i++) {
+      myServo.write(i);
+      delay(15); 
+    }
+    posisiGarasi = 90; // Simpan status bahwa garasi sekarang terbuka
+    Serial.println("Pintu Garasi Selesai DIBUKA.");
+   }
+}
+
+// Ketik 4: Tutup Garasi secara perlahan
+else if (hasil == '4') {
+  if (posisiGarasi != 0) {
+    Serial.println("Perintah: Menutup Pintu Garasi secara perlahan...");
+
+    //Loop menurunkan sudut dari posisi terakhir kembali ke 0 derajat
+    for (int i = posisiGarasi; i >=0; i--) {
+      myServo.write(i);
+      delay(15); // Beri jeda yang sama agar kecepatan tutup sewajarnya
+    }
+    posisiGarasi = 0; // Simpan status bahwa garasi sekarang tertutup
+    Serial.println("Pintu Garasi Selesai Ditutup.");
+  }
+}
+
 //LED LAMPU CONTROL
 //Ketik 2 :nyalakan lampu LED putih
 else if(hasil == '2'){
@@ -60,6 +108,8 @@ else if (hasil == '5'){
   Serial.println("Perintah: semua lampu DIMATIKAN");
   digitalWrite(ledPutih, LOW);
   digitalWrite(ledBening, LOW);
+ }
 }
 
+delay(20);
 }
